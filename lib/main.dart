7e7053c04669a88fa6ae5514cdf3f32ffc5baeba
Lucas Silva Silvestre <lucas_silvestre.10@hotmail.com>
2021-08-19 21:11:00 -0300

    Adicionado função para roletar dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,44 @@ void main() {
   );
 }
 
-class Dadoos extends StatelessWidget {
+class Dadoos extends StatefulWidget {
+  @override
+  _DadoosState createState() => _DadoosState();
+}
+
+class _DadoosState extends State<Dadoos> {
+  int numeroDadoEsquerda = 1;
+  int numeroDadoDireita = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  numeroDadoEsquerda = Random().nextInt(6) + 1;
+                  print(numeroDadoEsquerda);
+                });
+              },
+              child: Image.asset('assets/dado$numeroDadoEsquerda.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  numeroDadoDireita = Random().nextInt(6) + 1;
+                  print(numeroDadoDireita);
+                });
+              },
+              child: Image.asset('assets/dado$numeroDadoDireita.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
